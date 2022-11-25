455bea86cdb95428ccf2e6aed7fef7dbc26f94e3
krausediego <diegoemanuelk@gmail.com>
2022-11-24 22:16:32 -0300

    feat: Change service

--- packages/api/src/domain/services/create-user.ts
+++ packages/api/src/domain/services/create-user.ts
@@ -7,32 +7,28 @@ export class CreateUserService implements CreateUser {
   ) {}
 
   async run(params: CreateUser.Params): Promise<CreateUser.Response> {
-    try {
-      const { email, password, username } = params;
+    const { email, password, username } = params;
 
-      if (!email) {
-        throw new Error('O campo de email é obrigatório');
-      }
-
-      if (!password) {
-        throw new Error('O campo de senha é obrigatório');
-      }
-
-      if (!username) {
-        throw new Error('O campo username é obrigatório');
-      }
-
-      const passwordHashed = await this.hash.createHash(password);
-
-      await this.createUser.createNewUser({
-        email,
-        username,
-        password: passwordHashed,
-      });
-
-      return { message: 'Usuario criado com sucesso' };
-    } catch (error: any) {
-      throw new error(error);
+    if (!email) {
+      throw new Error('O campo de email é obrigatório');
     }
+
+    if (!password) {
+      throw new Error('O campo de senha é obrigatório');
+    }
+
+    if (!username) {
+      throw new Error('O campo username é obrigatório');
+    }
+
+    const passwordHashed = await this.hash.createHash(password);
+
+    await this.createUser.createNewUser({
+      email,
+      username,
+      password: passwordHashed,
+    });
+
+    return { message: 'Usuario criado com sucesso' };
   }
 }
--- packages/api/src/infra/prisma/repositories/create-user.ts
+++ packages/api/src/infra/prisma/repositories/create-user.ts
@@ -1,12 +1,21 @@
 import { ICreateUser } from '@/domain/interfaces';
 import { User } from '@prisma/client';
+import { Prisma as PrismaError } from '@prisma/client';
 import { Prisma } from '../prisma';
 export class CreateUser implements ICreateUser {
-  public async createNewUser(user: Omit<User, 'id'>): Promise<void> {
+  constructor() {}
+
+  async createNewUser(user: Omit<User, 'id'>): Promise<void> {
     try {
       await Prisma.user.create({ data: user });
     } catch (error: any) {
-      throw new Error(error.message);
+      if (error instanceof PrismaError.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new Error('Algum dos campos ja esta em uso');
+        } else {
+          throw new Error('Erro inesperado');
+        }
+      }
     }
   }
 }
