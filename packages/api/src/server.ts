00f839d96592bb4bde31868e7a2a0b4dfe7ac134
krausediego <diegoemanuelk@gmail.com>
2023-02-21 17:37:11 -0300

    feat: Change server.ts location.

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -2,6 +2,6 @@ import '@/main/config/dotenv';
 import env from '@/main/config/environments/application';
 
 Promise.all([]).then(async () => {
-  const app = (await import('./config/app')).default;
+  const app = (await import('./main/config/app')).default;
   await Promise.all([app.listen(env.port)]);
 });
